a4d12c1910f6c6be9e53187283a621e553764542
Pavel Kulikov <kulikovpavel@gmail.com>
2018-05-24 22:15:13 +0300

    Update attacker.py

--- attacker.py
+++ attacker.py
@@ -212,7 +212,7 @@ def main():
 
 
     attacker = FGSM_Attacker(model,
-                        eps=1e-3,
+                        eps=5e-3,
                         ssim_thr=SSIM_THR,
                         transform=transform,
                         img2tensor=img2tensor,
