0dced5c8163f76eaa3998ee7d11d5f667eed309b
Pavel Kulikov <kulikovpavel@gmail.com>
2018-05-29 08:50:52 +0300

    Update main.py

--- student_net_learning/main.py
+++ student_net_learning/main.py
@@ -274,6 +274,8 @@ def main():
     # Choosing of criterion
     if args.criterion == 'MSE':
         criterion = nn.MSELoss()
+    elif args.criterion == 'L1':
+        criterion = nn.L1Loss()()    
     else:
         criterion = None # Add your criterion
 
